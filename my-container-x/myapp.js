a704a0ef12ed956116dbdcc0e02a71eb8ba25394
Joseph Jhon Sabana <seffy.sabana@gmail.com>
2025-04-10 16:36:30 +1000

    update 310

---
+++ my-container-x/myapp.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const os = require('os');
+
+const listenPort = 8080;
+
+console.log('Server starting on host '+os.hostname()+' port '+listenPort+'...');
+const server = http.createServer((req, res) => {
+    let clientIP = req.connection.remoteAddress;
+    console.log('Processing request for '+req.url+' from '+clientIP);
+    res.writeHead(200);
+    res.end('Hello '+clientIP+', this is '+os.hostname()+'\n');
+});
+
+server.listen(listenPort);
